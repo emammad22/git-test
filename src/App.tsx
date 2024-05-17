741079218728ff201124f566bea5824d1c6bbed4
emammad22 <mammadoveltun22@gmail.com>
2024-05-17 16:32:01 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+export default function App() {
+
+  const sum = (a : number, b : number) : number=>{
+    return a + b
+  }
+
+  console.log(sum('12',5));
+
+  return (
+    <div>App</div>
+  )
+}
